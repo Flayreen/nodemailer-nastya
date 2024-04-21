edd194e57f9823cc1d4328c575b932ebebba81cc
Oleg Vakarchuk <vakarchuk.olegg@gmail.com>
2024-04-21 20:52:11 +0300

    add headers

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const mailSender = require("./send-mail/sender");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
 
 
 
